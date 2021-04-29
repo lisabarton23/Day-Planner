1cb25815cddc7135859803ce4f4925d721d7c53f
Lisa Barton <lisabarton23@yahoo.com>
2021-04-29 17:32:55 -0600

    debugging local storage get items

--- assets/script.js
+++ assets/script.js
@@ -49,7 +49,7 @@ $(".saveBtn").on ("click", function () {
 
 });
 
-$("#hour-9 .description").val (localStorage.getItem( "hour-9"));
+$("#hour-09 .description").val (localStorage.getItem( "hour-09"));
 $("#hour-10 .description").val (localStorage.getItem( "hour-10"));
 $("#hour-11 .description").val (localStorage.getItem( "hour-11"));
 $("#hour-12 .description").val (localStorage.getItem( "hour-12"));
