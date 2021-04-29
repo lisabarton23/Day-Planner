c9530b98fd526254769b7855d99cb1e8134fab10
Lisa Barton <lisabarton23@yahoo.com>
2021-04-29 16:03:17 -0600

    updated my functions with the class review

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,58 @@ date.text (moment ().format ("dddd, MMMM Do YYYY"));
 
 
 
+function checkTime () {
+
+var currentHour = moment().hours();
+
+$(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr ("id").split ("-")[1]);
 
 
+
+    if (blockHour < currentHour) {
+
+        $(this).addClass ("past")
+    } 
+    else if (blockHour === currentHour) {
+        $(this).addClass ("present")
+}
+else if (blockHour > currentHour) {
+
+    $(this).addClass ("future")
+}
+
+})
+
+
+
+
+
+}; 
+
+checkTime();
 
 
 $(".saveBtn").on ("click", function () {
 
     var value = $(this).siblings (".description").val ();
 
- 
+  var time = $(this).parent ().attr("id");
+  localStorage.setItem (time, value);
+  
 
-})
+});
+
+$("#hour-9 .description").val (localStorage.getItem( "hour-9"));
+$("#hour-10 .description").val (localStorage.getItem( "hour-10"));
+$("#hour-11 .description").val (localStorage.getItem( "hour-11"));
+$("#hour-12 .description").val (localStorage.getItem( "hour-12"));
+$("#hour-13 .description").val (localStorage.getItem( "hour-13"));
+$("#hour-14 .description").val (localStorage.getItem( "hour-14"));
+$("#hour-15 .description").val (localStorage.getItem( "hour-15"));
+$("#hour-16 .description").val (localStorage.getItem( "hour-16"));
+$("#hour-17 .description").val (localStorage.getItem( "hour-17"));
+
 
 })
 
